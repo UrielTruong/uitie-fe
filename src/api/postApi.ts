8674132113da5e19207461a35f72f9c8b8c05b24
Tri Le <83271916+trilm83@users.noreply.github.com>
2026-05-15 08:43:51 +0000

    feat: Tương tác bài viết (Like, Comment (bao gồm file đính kèm), Reply, Share.)

--- src/api/postApi.ts
+++ src/api/postApi.ts
@@ -2,25 +2,23 @@ import type { Pagination } from '#/types/response'
 import type { CreatePostPayload, Post, UpdatePostPayload } from '../types/post'
 import axiosClient from './axiosClient'
 
-interface CreatePostResponse {
+// --- Interface phản hồi cơ bản ---
+interface BaseResponse<T> {
   status: boolean
   message: string
-  data: Post
-}
-interface UpdatePostResponse {
-  status: boolean
-  message: string
-  data: Post
+  data: T
 }
 
-interface DeletePostResponse {
-  status: boolean
-  message: string
-}
+type CreatePostResponse = BaseResponse<Post>
+type UpdatePostResponse = BaseResponse<Post>
+type DeletePostResponse = Omit<BaseResponse<any>, 'data'>
+
+// --- Post API ---
+
 export const fetchFeed = async (): Promise<Post[]> => {
-    const res = await axiosClient.get('/post')
-    return res.data.data
-  }
+  const res = await axiosClient.get('/post')
+  return res.data.data
+}
 
 export const createPost = (payload: CreatePostPayload) =>
   axiosClient
@@ -29,7 +27,7 @@ export const createPost = (payload: CreatePostPayload) =>
 
 export const searchPost = async (keyword?: string): Promise<Pagination<Post[]>> => {
   const res = await axiosClient.get<Pagination<Post[]>>('/post/search', { params: { keyword } })
-  return res.data  
+  return res.data
 }
 
 export const updatePost = (id: number, payload: UpdatePostPayload) =>
@@ -41,3 +39,46 @@ export const deletePost = (id: number) =>
   axiosClient
     .delete<DeletePostResponse>(`/post/${id}`)
     .then((res) => res.data)
+
+// --- Tương tác bài viết (Thêm mới) ---
+
+/**
+ * Like hoặc Unlike bài viết
+ */
+export const likePost = (id: number) =>
+  axiosClient
+    .post<BaseResponse<any>>(`/post/${id}/like`)
+    .then((res) => res.data)
+
+/**
+ * Chia sẻ bài viết
+ */
+export const sharePost = (id: number) =>
+  axiosClient
+    .post<BaseResponse<any>>(`/post/${id}/share`)
+    .then((res) => res.data)
+
+/**
+ * Bình luận bài viết (Hỗ trợ file đính kèm và Reply)
+ * Dùng FormData để truyền dữ liệu đa phương thức
+ */
+export const createComment = (formData: FormData) =>
+  axiosClient
+    .post<BaseResponse<any>>('/comment', formData, {
+      headers: {
+        'Content-Type': 'multipart/form-data',
+      },
+    })
+    .then((res) => res.data)
+
+// Gom nhóm lại để dễ sử dụng nếu cần
+export const postApi = {
+  fetchFeed,
+  createPost,
+  searchPost,
+  updatePost,
+  deletePost,
+  likePost,
+  sharePost,
+  createComment,
+}
